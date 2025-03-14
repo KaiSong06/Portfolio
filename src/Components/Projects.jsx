38208544a90c35fabb04e5f796f7b5625309276c
KaiSong06 <Kai.Song421@gmail.com>
2025-03-14 00:54:51 -0400

    Attempt 3

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -29,7 +29,7 @@ const Projects = () => {
 
     return (
         <div className="relative flex justify-center items-center w-screen h-screen" id="projects">
-            <div className="absolute lg:top-0 lg:text-7xl md:top-15 md:text-4xl top-35 text-xl text-sky-500 font-bold">
+            <div className="absolute lg:top-0 lg:text-7xl md:top-15 md:text-4xl top-35 text-md text-sky-500 font-bold">
                 <h1>My Projects</h1>
             </div>
 
